c2760bfbefcaa8682312ee9507cc114df74e46bc
Adam Englishby <adam@englishby.ie>
2025-09-21 13:20:15 +0100

    cutout

---
+++ Assets/Code/Runtime/Camera/CutoutManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Adam.Runtime.Code.Runtime.Camera
+{
+    
+    public class CutoutManager : MonoBehaviour
+    {
+        private static readonly int CutoutPos = Shader.PropertyToID("_CutoutPos");
+
+        [SerializeField] private UnityEngine.Camera cam;
+        [SerializeField] private List<Material> materials;
+
+        private void Update()
+        {
+            var cutoutPos = cam.WorldToViewportPoint(transform.position);
+
+            foreach (var material in materials)
+            {
+                material.SetVector(CutoutPos, cutoutPos);
+            }
+        }
+    }
+    
+}
